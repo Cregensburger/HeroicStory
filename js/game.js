c4920f1ee217bd8c7ef26a9f4291f8fe115c666f
Courtney Regensburger <courtneyregensburger@yahoo.com>
2015-06-22 15:00:38 -0500

    JS pseudocode WIP

---
+++ js/game.js
@@ -0,0 +1,80 @@
+//Button to start game, leads to intro message
+//directed at champion on light
+
+
+//Name input submission form, take value and make a var for 
+//later access. Must be at least five alphanumeric letters long,
+//make sure that it's the name the user wants. If not clear value 
+//and let them input a new name, rinse and repeat until confirmed.
+//Once submitted go to gender picker
+
+
+//two clickable images, male or female, make catch the value 
+//for later use, ask if certain. Once confirmed, move onto
+//race options. 
+
+
+//Provide race pictures. If male, show male elf, human, and dwarf.
+//If female, show female elf, human, and dwarf. Let user select
+//and get their confirmation.
+
+/*FOR HUMAN*/
+//You slept with farmer's daughter. Choose bucket, words, or chicken image.
+
+	//Bucket
+		// The mighty bucket! The often overlooked weapon of champions.
+		// You take a swing. How much force do you use?
+		//A fluctuating power bar(0-100). On clicking enter, stop moving and
+		//find value. Pop up numerical value and switch to according scenario
+			//For values between 0-33
+			//A pathetic swing! Were you even trying? You fail so horribly,
+			//the farmer actually feels bad for you. After an awkward apology
+			//you go on your way, taking the bucket with you. 
+			//Congratulations? How do you celebrate?
+				//Head into the wilderness
+					//You go on a fantastically magical adventure with the bucket.
+					//As it turns out, the bucket has always dreamed of a life like this.
+					//As time goes on, the two of you become closer. More than just a pair of 
+					//adventurers, but life partners. After swearing off Farmer's Daughters for
+					//forever, you and the bucket start a new life together. The adventure of 
+					//marriage. You live happiy ever after with bucket and your two pail children.
+					//The end button (return to start)
+				//Go to the next town over
+					//You discard the bucket and head to the next town over. Suffering from
+					//a powerful thirst, you hit up the pub. Next thing you know, you're
+					//making eyes at a pretty young thing seated next to you. You ask her who she is.
+					//With a smile, she replies "Why, I'm the farmer's daughter."
+					//Button: Here we go again... (return to start)
+
+	//Words
+
+
+	//Chickens
+
+
+
+/*FOR DWARF*/
+//Wake up after binge drinking to an inn on fire. Choose run, dig or extinguish image.
+
+	//Run
+
+
+	//Dig
+
+
+	//Extinguish
+
+
+
+/*FOR ELF*/
+//Find a white stag while in forest. Choose leave, help, or kill image.
+
+	//Leave
+
+
+	//Help
+
+
+	//Kill
+
+
